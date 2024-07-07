b7c163e7f996b1fe008f726555705690c7a858be
JJuHS <139028940+JJuHS@users.noreply.github.com>
2024-07-08 08:51:57 +0900

    [Gold IV] Title: 두 배, Time: 268 ms, Memory: 60908 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.py
+++ 백준/Gold/31963. 두 배/두 배.py
@@ -1,11 +1,19 @@
+import math
+import sys; input = sys.stdin.readline
+
+
+def sol(arr, n):
+    tmp = 0
+    cnt_arr = [0] * n
+    for i in range(1, n):
+        tmp_cnt = math.ceil(math.log2(arr[i - 1] / arr[i])) + cnt_arr[i - 1]
+        if tmp_cnt > 0:
+            cnt_arr[i] = max(0, math.ceil(math.log2(arr[i - 1] / arr[i])) + cnt_arr[i - 1])
+            tmp += cnt_arr[i]
+    return tmp
+
+
 n = int(input())
 arr = list(map(int, input().split()))
-cnt = 0
-for i in range(n - 1):
-    while True:
-        if arr[i] > arr[i + 1]:
-            arr[i + 1] *= 2
-            cnt += 1
-            continue
-        break
-print(cnt)
+
+print(sol(arr, n))
