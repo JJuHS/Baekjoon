5cfe1eb814fa9c5adfbf0548d0e25ce6b9560883
JJuHS <139028940+JJuHS@users.noreply.github.com>
2024-07-16 09:38:07 +0900

    [Gold V] Title: 맨해튼에서의 모임, Time: 436 ms, Memory: 120348 KB -BaekjoonHub

--- 백준/Gold/27297. 맨해튼에서의 모임/맨해튼에서의 모임.py
+++ 백준/Gold/27297. 맨해튼에서의 모임/맨해튼에서의 모임.py
@@ -1,13 +1,14 @@
-import sys; input = sys.stdin.readline
+import sys;input = sys.stdin.readline
+
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(m)]
 ans1, ans2 = 0, []
 
 for i in range(n):
-    tmp = [arr[j][i] for j in range(m)]
-    tmp.sort()
-    ans1 += sum([abs(tmp[k] - tmp[m//2]) for k in range(m)])
-    ans2.append(tmp[m//2])
-    
+    tmp = sorted([arr[j][i] for j in range(m)])
+    c = tmp[m // 2]
+    ans1 += sum([abs(tmp[k] - c) for k in range(m)])
+    ans2.append(c)
+
 print(ans1)
 print(*ans2)
