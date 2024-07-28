fc32cec2cdd301a9c83f2648d15cc8e7d82cba0d
JJuHS <139028940+JJuHS@users.noreply.github.com>
2024-07-29 08:49:48 +0900

    [Gold V] Title: 가로등, Time: 240 ms, Memory: 31120 KB, Score: 20 point -BaekjoonHub

--- 백준/Gold/32069. 가로등/가로등.py
+++ 백준/Gold/32069. 가로등/가로등.py
@@ -1,13 +1,15 @@
 import sys;input = sys.stdin.readline
 
 l, n, k = map(int, input().split()) # 도로 길이, 가로등 개수, 출력개수
-arr = list(map(int, input().split())) # 가로등 배열
+arr = list(map(int, input().split()) ) # 가로등 배열
 
 visit = []
 res = ''
 total = 0
 idx = 0
 while True:
+    if total == k:
+        break
     for i in arr:
         if i + idx <= l and (i + idx) not in visit:
             visit.append(i + idx)
@@ -21,8 +23,6 @@ while True:
             total += 1
         if total == k:
             break
-    if total == k:
-        break
     idx += 1
     
 print(res)
