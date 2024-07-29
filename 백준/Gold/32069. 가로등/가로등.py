9f4b9b0059991ea00f4949293de01bfdfda0aee3
JJuHS <139028940+JJuHS@users.noreply.github.com>
2024-07-29 09:39:40 +0900

    [Gold V] Title: 가로등, Time: 276 ms, Memory: 168224 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/32069. 가로등/가로등.py
+++ 백준/Gold/32069. 가로등/가로등.py
@@ -1,31 +1,28 @@
-import sys
-input = sys.stdin.readline
+import sys; input = sys.stdin.readline
 
 l, n, k = map(int, input().split())
 arr = list(map(int, input().split()))
+
 if n >= k:
     print('0\n' * k)
     exit()
-visit = []
-res = ''
-distance = 0
-flag = True
-while flag:
-    for now in arr:
-        if now + distance <= l and (now + distance) not in visit:
-            visit.append(now + distance)
-            res += str(distance) + '\n'
-            k -= 1
-            if k == 0:
-                flag = False
-                break
-        if now - distance >= 0 and (now - distance) not in visit:
-            visit.append(now - distance)
-            res += str(distance) + '\n'
-            k -= 1
-            if k == 0:
-                flag = False
-                break
 
-    distance += 1
-print(res)
+k -= n
+print('0\n' * n, end='')
+dist = 1
+
+while True:
+    dist_str = str(dist)
+    for tmp in range(n):
+        if (not tmp and arr[tmp] >= dist) or (tmp and arr[tmp] > arr[tmp - 1] + 2 * dist - 1):
+            print(dist_str)
+            k -= 1
+        if not k:
+            exit()
+        if (tmp == n - 1 and arr[tmp] <= l - dist) or (tmp != n - 1 and arr[tmp] < arr[tmp + 1] - 2 * dist):
+            print(dist_str)
+            k -= 1
+        if not k:
+            exit()
+    dist += 1
+
