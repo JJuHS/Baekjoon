87415e882ada882a603609eeb6f316e411191122
JJuHS <139028940+JJuHS@users.noreply.github.com>
2024-08-04 00:06:30 +0900

    [Silver III] Title: queuestack, Time: 220 ms, Memory: 48496 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -0,0 +1,20 @@
+import sys;input = sys.stdin.readline
+from collections import deque 
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+m = int(input())
+c = list(map(int, input().split()))
+
+q = deque()
+for i in range(n):
+    if a[i] == 0:
+        q.appendleft(b[i])
+else:
+    if not q:
+        print(*c)
+        exit(0)
+
+for i in range(m):
+    q.append(c[i])
+    print(q.popleft(), end=' ')
