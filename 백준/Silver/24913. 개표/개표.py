bcb281397e67f3d1e0052252435c11738f4c14ae
Jung HoSung <139028940+JJuHS@users.noreply.github.com>
2025-01-05 22:07:45 +0900

    [Silver II] Title: 개표, Time: 56 ms, Memory: 31120 KB, Score: 10 point -BaekjoonHub

--- 백준/Silver/24913. 개표/개표.py
+++ 백준/Silver/24913. 개표/개표.py
@@ -1,28 +1,16 @@
-import sys;input = sys.stdin.readline
-
-def sol1(x, p):
-    candidates[p] += x
-    return
-
-def sol2(x, y):
-    my = candidates[n] + x
-
-    cnt = 0
-    for i in range(n):
-        if my > candidates[i]:
-            cnt += my - candidates[i] - 1
-        else: 
-            print('NO')
-            return
-        
-    print('YES') if cnt >= y else print('NO')
-
+import sys; input = sys.stdin.readline
 n, q = map(int, input().split())
-candidates = [0] * (n + 1)
-
-for _ in range(q):
+votes = [0] * (n + 2)
+for i in range(q):
     a, b, c = map(int, input().split())
     if a == 1:
-        sol1(b, c - 1) 
-    else:
-        sol2(b, c)
+        votes[c] += b
+    if a == 2:
+        my_vote = votes[-1] + b
+        res = 0
+        for vote in votes[1:n+1]:
+            if my_vote > vote + c:
+                res = 1
+                break
+        print(['NO', 'YES'][res])
+
