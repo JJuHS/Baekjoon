19b982cd4e049ebf20ac3b3eb8d9b131430550d9
Jung HoSung <139028940+JJuHS@users.noreply.github.com>
2025-01-09 15:44:45 +0900

    [Gold IV] Title: 트리의 지름, Time: 68 ms, Memory: 37992 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,28 +1,26 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+import sys;input = sys.stdin.readline
+sys.setrecursionlimit(1000000)
 
 n = int(input())
 graph = [[] for _ in range(n + 1)]
-for _ in range(n-1):
+for _ in range(n - 1):
     p, c, w = map(int, input().split())
     graph[p].append([c, w])
     graph[c].append([p, w])
 
-
-def dfs(x = 1, dist=0):
-    for end, weight in graph[x]:
-        if ans[end] == -1:
-            ans[end] = dist + weight
-            dfs(end, dist + weight)
+def dfs(x:int=1, d:int=0):
+    for e, w in graph[x]:
+        if ans[e] == -1:
+            ans[e] = d + w
+            dfs(e, d + w)
 
 ans = [-1] * (n + 1)
 ans[1] = 0
 dfs()
-start = ans.index(max(ans))
+s = ans.index(max(ans))
 
 ans = [-1] * (n + 1)
-ans[start] = 0
+ans[s] = 0
+dfs(s)
 
-dfs(start)
 print(max(ans))
