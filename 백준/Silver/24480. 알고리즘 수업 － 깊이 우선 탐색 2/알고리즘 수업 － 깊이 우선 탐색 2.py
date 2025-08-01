143d032d76d26607af27b5d9c180b05ee502ce2d
Jung HoSung <139028940+JJuHS@users.noreply.github.com>
2025-08-01 23:26:31 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 464 ms, Memory: 69288 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,26 @@
+import sys; input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n, m, r = map(int, input().split())
+
+arr = [[] for _ in range(n + 1)]
+for _ in range(m):
+    u, v = map(int, input().split())
+    arr[u].append(v)
+    arr[v].append(u)
+    
+visit = [0] * (n + 1)
+now = 1
+
+def dfs(x):
+    global now
+    visit[x] = now
+    arr[x].sort(reverse=True)
+    for a in arr[x]:
+        if not visit[a]:
+            now += 1
+            dfs(a)
+dfs(r)
+
+for i in range(n):
+    print(visit[i+1])
